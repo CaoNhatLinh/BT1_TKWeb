35769cd3bf9a6233b9235b6a23aef683534fe7cd
nhatlinh <caonhatlinh1312@gmail.com>
2023-03-13 15:38:55 +0700

    fix bug tag a

--- assets/index.js
+++ assets/index.js
@@ -63,3 +63,11 @@ expandBtns.forEach( (btn,index) => {
 
 
 
+$('.lazy').Lazy({
+  scrollDirection: 'vertical',
+  effect: 'fadeIn',
+  visibleOnly: true,
+  onError: function(element) {
+      console.log('error loading ' + element.data('src'));
+  }
+});
